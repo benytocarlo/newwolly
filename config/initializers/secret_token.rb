ceead96c2f2680c7f0320ddff0d3c12a261019b6
benytocarlo <itoknot2004@gmail.com>
2013-09-18 23:08:59 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wollyv2::Application.config.secret_token = '037eb4c41fb59338778b0aff6c9e1600f43f540145da20877452dc7d392bd869b654104b0f0fc9f5a8978df0ab2fcc783c7921430ce202fcc71e8b6dc7ade5aa'
